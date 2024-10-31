f5579cdffd62e26b95d78d7f62b9095df3a0fbaf
Alireza Asghari <alireza.asgharii24@gmail.com>
2024-10-31 08:36:24 +0000

    💄 Create signup page

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,11 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
+  const isAuthenticated = false;
+
+  if (isAuthenticated) return <Navigate to="/" />;
   return (
-    <div>
-      AuthLayout
-      <Outlet />
-    </div>
+    <>
+      <section className="flex flex-1 justify-center items-center flex-col py-10 overflow-y-auto">
+        <Outlet />
+      </section>
+      <img
+        src="/assets/images/side-img.svg"
+        alt="logo"
+        className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+      />
+    </>
   );
 };
 
