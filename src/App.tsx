24a2dfd22b27b10e6f21dc03c19c6f38444f832d
Alireza Asghari <alireza.asgharii24@gmail.com>
2024-11-03 09:52:18 +0000

    ✨ Add toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import Router from "./router/Router";
+import { Toaster } from "./components/ui/toaster";
 
 function App() {
   return (
-    <main className="flex h-screen">
-      <Router />
-    </main>
+    <>
+      <main className="flex h-screen">
+        <Router />
+      </main>
+      <Toaster />
+    </>
   );
 }
 
