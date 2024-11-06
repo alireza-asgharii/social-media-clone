d5208458611aca5f5aa6f4ca0086291616625589
Alireza Asghari <alireza.asgharii24@gmail.com>
2024-11-06 09:58:01 +0000

    ✨ Create mobile bottombar

--- src/components/shared/BottomBar.tsx
+++ src/components/shared/BottomBar.tsx
@@ -1,5 +1,33 @@
+import { Link, useLocation } from "react-router-dom";
+
+import { sidebarLinks } from "../../constants";
+
 const BottomBar = () => {
-  return <div></div>;
+  const { pathname } = useLocation();
+  return (
+    <section className="bottom-bar">
+      {sidebarLinks.map((link) => {
+        const isActive = pathname === link.route;
+
+        return (
+          <Link
+            to={link.route}
+            key={link.label}
+            className={`${isActive ? "bg-primary-500" : ""} flex-center flex-col gap-1 rounded-[10px] p-2 transition`}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              className={` ${isActive && "invert-white"}`}
+              width={16}
+              height={16}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
 };
 
 export default BottomBar;
