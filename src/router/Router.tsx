091e41e23eb7da9c6c657075d2a84269d155577e
Alireza Asghari <alireza.asgharii24@gmail.com>
2024-11-06 08:50:21 +0000

    ✨ Create pages

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,5 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "../_root/pages";
+
+//pages
+import {
+  AllUser,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "../_root/pages";
+
 import SigninForm from "../_auth/forms/SigninForm";
 import SignupForm from "../_auth/forms/SignupForm";
 import RootLayout from "../_root/RootLayout";
@@ -17,6 +30,14 @@ const Router = () => {
       {/* privte routes */}
       <Route element={<RootLayout />}>
         <Route index element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/saved" element={<Saved />} />
+        <Route path="/all-users" element={<AllUser />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/update-post/:id" element={<EditPost />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
+        <Route path="/profile/:id/*" element={<Profile />} />
+        <Route path="/update-profile/:id" element={<UpdateProfile />} />
       </Route>
     </Routes>
   );
