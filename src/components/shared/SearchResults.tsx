aa7d949d837e69414429119dfdb8f2b4d08a9d8f
Alireza Asghari <alireza.asgharii24@gmail.com>
2024-11-16 09:07:08 +0000

    ✨ Create search result section

--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -1,5 +1,21 @@
-const SearchResults = () => {
-  return <div></div>;
+import { Models } from "appwrite";
+import Spiner from "./Spinner";
+import GridPostList from "./GridPostList";
+
+type props = {
+  searchPosts?: Models.Document[];
+  isSearchFetching: boolean;
+};
+
+const SearchResults = ({ isSearchFetching, searchPosts }: props) => {
+  if (isSearchFetching) return <Spiner />;
+
+  if (searchPosts && searchPosts?.length > 0)
+    return <GridPostList posts={searchPosts} />;
+
+  return (
+    <p className="mt-10 w-full text-center text-light-4">No result found</p>
+  );
 };
 
 export default SearchResults;
